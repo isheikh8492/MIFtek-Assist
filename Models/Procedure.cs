af94bfc2b1a79c419a35c4cc51c9bb833c946f90
isheikh8492 <60596395+isheikh8492@users.noreply.github.com>
2024-09-24 17:23:09 -0500

    project init

---
+++ Models/Procedure.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MIFtek_Assist.Models
+{
+    public class Procedure
+    {
+        public string Title { get; set; } // Title of the procedure
+        public ObservableCollection<string> Steps { get; set; } // Steps in the procedure
+
+        public Procedure(string title)
+        {
+            Title = title;
+            Steps = new ObservableCollection<string>();
+        }
+    }
+}
